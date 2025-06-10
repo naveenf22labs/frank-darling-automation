5faa2395cf31c5728f312fc2698ecba74bb83ae4
naveenf22labs <naveen.bachu@f22labs.com>
2025-06-10 16:52:38 +0530

    Pause the schedule time

--- src/test/java/utils/com/FDUtils.java
+++ src/test/java/utils/com/FDUtils.java
@@ -34,7 +34,7 @@ public class FDUtils
 		//System.out.println(" Launching browser in @BeforeMethod");
 		ChromeOptions options = new ChromeOptions();
 		 options.addArguments("--incognito");
-		options.addArguments("--headless=new");
+		 options.addArguments("--headless=new");
 		options.addArguments("--no-sandbox");
         	options.addArguments("--disable-dev-shm-usage");
         	options.addArguments("--window-size=1920,1080");
